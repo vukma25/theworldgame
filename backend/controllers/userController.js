08b86cf26b852d8055ed00fb69e21715e355a86e
hoàng vũ <tuanvu.actvn.edu@gmail.com>
2025-11-14 11:33:35 +0700

    maintain

--- backend/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-
-const userController = {
-    fetchUser: async (req, res) => {
-        const user = req.user._doc
-        return res.status(200).json({ user })
-    }
-}
-
-export default userController
